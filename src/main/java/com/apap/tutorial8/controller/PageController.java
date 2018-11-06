10cb1a5acc4841cf16e8bcfd0c6adfd917b63fe3
faisal ridwan <faisal.ridwan55@gmail.com>
2018-11-06 18:50:01 +0700

    add role status @homePage

--- src/main/java/com/apap/tutorial8/controller/PageController.java
+++ src/main/java/com/apap/tutorial8/controller/PageController.java
@@ -1,12 +1,24 @@
 package com.apap.tutorial8.controller;
 
+import com.apap.tutorial8.model.UserRoleModel;
+import com.apap.tutorial8.service.UserRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
+    @Autowired
+    private UserRoleService userService;
+
     @RequestMapping("/")
-    public String home() {
+    public String home(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserRoleModel user = userService.findByUsername(authentication.getName());
+        model.addAttribute("role", user.getRole().toLowerCase());
         return "home";
     }
 
